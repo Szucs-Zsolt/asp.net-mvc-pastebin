6d4b43d86f578496d9f2cf8474be1b9dffcd95cf
Szücs Zsolt <szucszsolt.hu@gmail.com>
2023-08-21 11:29:57 +0200

    Adatbázis létrehozásakor default üzenet, amit Index beolvas MessageId alapján.

--- PastebinhezHasonlo/Controllers/HomeController.cs
+++ PastebinhezHasonlo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PastebinhezHasonlo.Data;
 using PastebinhezHasonlo.Models;
 using System.Diagnostics;
 
@@ -7,16 +8,42 @@ namespace PastebinhezHasonlo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        // Dependency injection: EntityFramework
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(
+            ILogger<HomeController> logger,
+            ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         // GET
         public IActionResult Index()
         {
             return View(new Message());    
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string messageId)
+        {
+            var result = new Message();
+
+            if (string.IsNullOrEmpty(messageId))
+            {
+                result.Msg = "Hiányzik az üzenetazonosító.";
+                return View(result);
+            }
+
+            result = _db.Messages.FirstOrDefault(x => x.MessageId == messageId);
+            if (result == null)
+            {
+                result = new Message() { Msg = "Nincs ilyen azonosítójú üzenet."};
+            }
+            return View(result);
         }
 
         public IActionResult Privacy()
--- PastebinhezHasonlo/Data/ApplicationDbContext.cs
+++ PastebinhezHasonlo/Data/ApplicationDbContext.cs
@@ -12,6 +12,18 @@ namespace PastebinhezHasonlo.Data
         }
 
         // Táblák
-        DbSet<Message> Messages { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        // Alapértékkel feltöltjük, hogy legyen min tesztelni.
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Message>().HasData(
+                new Message() { Id = 1, MessageId = "1", Msg = "Példaüzenet." },
+                new Message() { Id = 2, MessageId = "2", Msg = "Második üzenet."}
+                );
+        }
     }
 }
