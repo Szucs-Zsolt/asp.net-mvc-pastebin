9ecbf30b501383fbb9a884c5682fbb75f3c9ce68
Szücs Zsolt <szucszsolt.hu@gmail.com>
2023-08-21 10:16:16 +0200

    First commit.

---
+++ PastebinhezHasonlo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PastebinhezHasonlo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
