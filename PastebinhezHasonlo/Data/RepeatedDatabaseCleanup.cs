26aab7f2e8489aacc2761a7b848f9c435aa4a78d
Szücs Zsolt <szucszsolt.hu@gmail.com>
2023-08-23 10:52:52 +0200

    Óránként törli az adatbázisból a lejárt üzeneteket.

--- PastebinhezHasonlo/Data/RepeatedDatabaseCleanup.cs
+++ PastebinhezHasonlo/Data/RepeatedDatabaseCleanup.cs
@@ -1,15 +1,30 @@
-﻿namespace PastebinhezHasonlo.Data
+﻿using PastebinhezHasonlo.Models;
+
+namespace PastebinhezHasonlo.Data
 {
     public class RepeatedDatabaseCleanup : BackgroundService
     {
-        // private readonly PeriodicTimer _timer = new PeriodicTimer(TimeSpan.FromHours(1));
-        private readonly PeriodicTimer _timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+        // Adatbázis elérése
+        private readonly ApplicationDbContext _db;
+        public RepeatedDatabaseCleanup(IServiceScopeFactory factory)
+        {
+            _db = factory.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>();       
+        }
+
+        // Általános karbantartás: Óránként ellenőrzi és törli a lejárt üzeneteket.
+        // Mivel ez nem teljesen pontos, ezért megjelenítés előtt is ellenőrizni fogjuk,
+        // hogy nem járt-e le egy üzenet ideje.
+        private readonly PeriodicTimer _timer = new PeriodicTimer(TimeSpan.FromHours(1));
         protected override async Task ExecuteAsync(CancellationToken stopToken)
         {
             while (await _timer.WaitForNextTickAsync(stopToken)
-                && !stopToken.IsCancellationRequested) 
+                && !stopToken.IsCancellationRequested)
             {
-                Console.WriteLine(DateTime.Now.ToString());
+                // Adatbázis tisztítása (ha már túl vagyunk a lejárati idején)
+                DateTime currentDateTime = DateTime.Now;
+
+                _db.Messages.RemoveRange(_db.Messages.Where(x => x.DiscardDate < currentDateTime));
+                _db.SaveChanges();
             }
         }
     }
