10275df0d16043a09074bc694a9d972183584082
Szücs Zsolt <szucszsolt.hu@gmail.com>
2023-08-26 15:31:48 +0200

    Admin más üzeneteit törölheti, vagy vissza az előző oldalra.

--- PastebinhezHasonlo/Models/Message.cs
+++ PastebinhezHasonlo/Models/Message.cs
@@ -8,7 +8,6 @@ namespace PastebinhezHasonlo.Models
         [Key]
         public int Id { get; set; }
         
-        
         [Display(Name ="Üzenetazonosító")]
         public string? MessageId { get; set; }
 
